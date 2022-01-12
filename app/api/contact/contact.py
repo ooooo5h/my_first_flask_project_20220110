065126ceb562b6aaf3f1e8a5f45979c2c69a0b29
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 12:57:21 +0900

    받아낸 파라미터를 이용해서 연락처 추가하는 쿼리만 작성

--- app/api/contact/contact.py
+++ app/api/contact/contact.py
@@ -1,4 +1,25 @@
-# 연락처와 관련된 모든 로직을 담당하는 파일
+from pymysql import connect
+from pymysql.cursors import DictCursor
 
+# 연락처와 관련된 모든 로직을 담당하는 파일
+# DB연결 / cursor 변수
+db = connect(
+    host='finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com',
+    port=3306,
+    user='admin',
+    passwd='Vmfhwprxm!123',
+    db='test_phone_book',
+    charset = 'utf8',  
+    cursorclass = DictCursor,
+)
+cursor = db.cursor()
+
+# 연락처 추가
 def add_contact_to_db(params):
-    pass
+    
+    sql = f"INSERT INTO contacts (user_id, name, phone_num, memo) VALUES ({params['user_id']}, '{params['name']}', '{params['phone']}','{params['memo']}')"
+    
+    return {
+        'code' : 200,
+        'message' : '임시 연락처 추가 성공 응답'
+    }
